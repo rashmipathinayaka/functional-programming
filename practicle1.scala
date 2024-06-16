31121338537afb9f4b74a496074d9647fffaa518
Rashmi Pathinayaka <140165202+rashmipathinayaka@users.noreply.github.com>
2024-06-16 11:40:13 +0530

    Add files via upload

---
+++ practicle1.scala
@@ -0,0 +1,16 @@
+//first question
+def area(r:Double)=math.Pi*r*r;
+
+//second question
+def temp(c:Double)=c*1.8+32;
+
+//third question
+def vol(r1:Double)=(4*math.Pi*r1*r1*r1)/3;
+
+//fourth question
+def total():Double=24.95-(24.95*0.4)+3+0.75*10;
+
+//fifth question
+def runtime():Double=2*8+3*7+2*8;
+
+
