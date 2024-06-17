1505c3df25e59d9809b3788044c8a47222111f03
Rashmi Pathinayaka <140165202+rashmipathinayaka@users.noreply.github.com>
2024-06-17 20:45:34 +0530

    Update practicle1.scala

--- practicle1.scala
+++ practicle1.scala
@@ -8,9 +8,13 @@ def temp(c:Double)=c*1.8+32;
 def vol(r1:Double)=(4*math.Pi*r1*r1*r1)/3;
 
 //fourth question
-def total():Double=24.95-(24.95*0.4)+3+0.75*10;
+def cost(a:Double)=(24.95-24.95*0.4)*a;
+def total(a:Double):Double=if(a>50){
+3+(a-50)*0.75+cost(a)
+ }else{ 
+3+cost(a)};
 
 //fifth question
-def runtime():Double=2*8+3*7+2*8;
-
+def runtimeeasypace(a:Double):Double=a*8;
+def runtimetempo(b:Double):Double=b*7;
 
